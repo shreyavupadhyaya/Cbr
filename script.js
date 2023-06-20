88685311b9fa6dd67cd2dd6ec6e326ed164e3f41
Shreya Upadhyaya <95024721+shreyavupadhyaya@users.noreply.github.com>
2023-06-21 00:18:21 +0530

    script.js

--- script.js
+++ script.js
@@ -5,18 +5,21 @@
       var output = "";
 
       if (input === "CBR21") {
-        output = "Welcome to CBR";
+        output = "www.hackerrank.com/end-game-03-1";
       } 
       else if(input === "SUK23") {
-        output = "Code Unlocked";
+        output = "www.hackerrank.com/end-game-04";
       }
     
       else if(input === "DBC04"){
-        output = "Code Unlocked";
+        output = "www.hackerrank.com/end-game-03";
       }
      
       else if(input === "HAK05"){
-        output = "Code Unlocked";
+        output = "www.hackerrank.com/end-game-01-1687249300";
+      }
+      else if(input === "HAK06"){
+        output = "www.hackerrank.com/end-game-02-1687249799";
       }
       else{
         output = "Try again! surprise awaits you";
